75d75ae8086352f3f003a67a16111416d9dea63f
yschoo0 <yyssms@daum.net>
2023-07-25 17:07:15 +0900

    라이브러리와 프레임워크

--- 생활/colors.js
+++ 생활/colors.js
@@ -1,19 +1,22 @@
 var Links = {
     setColor : function(color){
-    var alist = document.querySelectorAll('a');
-    var i = 0;
-    while(i < alist.length){
-        alist[i].style.color = color;
-        i = i + 1;
-    }
+    // var alist = document.querySelectorAll('a');
+    // var i = 0;
+    // while(i < alist.length){
+    //     alist[i].style.color = color;
+    //     i = i + 1;
+    // }
+    $('a').css('color',color);
 }
 }
 var Body = {
     setColor:function(color){
-        document.querySelector('body').style.color = color;
+        // document.querySelector('body').style.color = color;
+        $('body').css('color',color);
     },
     setBackgroundColor:function(color){
-        document.querySelector('body').style.backgroundColor = color;
+        // document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor',color);
     }
 }
 //코드를 효율적으로 바꾸는 걸 리팩토링이라고 함
@@ -23,7 +26,7 @@ function nightDayHandler(self){
     Body.setBackgroundColor('black');
     Body.setColor('white');
     self.value = 'day';
-    Links.setColor('white');
+    Links.setColor('gold');
 
     } else{
     Body.setBackgroundColor('white');
